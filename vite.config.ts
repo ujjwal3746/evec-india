833a6a29fcec1f2ef5ea05c98dceed61ad6189a8
ujjwal3746 <ujjwal3746@gmail.com>
2026-02-19 15:54:29 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // This allows the app to use process.env.API_KEY in the browser
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
     outDir: 'dist',
